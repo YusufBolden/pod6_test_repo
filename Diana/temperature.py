6645e2e4977010f5b6f931c4074649f360ad1c50
Diana <admin@admins-iMac.lan>
2021-02-02 16:49:45 -0800

    Updated temperature challenge

--- Diana/temperature.py
+++ Diana/temperature.py
@@ -1,37 +1,37 @@
 # Assigning variable for converting 100 degrees farenheit to celsius
-celsius_100 = (100-32) * (5/9)
+#celsius_100 = (100-32) * (5/9)
 
 #printing results of conversion 1
-print(celsius_100)
-print(type(celsius_100))
-print(int(celsius_100))
+#print(celsius_100)
+#print(type(celsius_100))
+#print(int(celsius_100))
 
 #updating variable for converting 0 degrees farenheit
-celsius_0 = (0-32) * (5/9)
+#celsius_0 = (0-32) * (5/9)
 #printing results of conversion 2
-print (celsius_0)
+#print (celsius_0)
 
 #printing conversion 3 without the use of a variable
-print ((34.2-32)) * (5/9)
+#print ((34.2-32)) * (5/9)
 
 #printing farenheit temperature for 5 degrees celsius
-farenheit_5 = (5*(9/5)+32)
-print (farenheit_5)
+#farenheit_5 = (5*(9/5)+32)
+#print (farenheit_5)
 
 #comparing 30.2 degrees celsius and 85.1 degrees farenheit by determining farenheit 30.2 degrees celsius
-celsius_30_2 = (30.2 * (9/5)+32)
-print (celsius_30_2)
-print ("30.2 degrees celsius is 86.36 farenheit, which is hotter than 85.1 degrees farenheit")
+#celsius_30_2 = (30.2 * (9/5)+32)
+#print (celsius_30_2)
+#print ("30.2 degrees celsius is 86.36 farenheit, which is hotter than 85.1 degrees farenheit")
 
 
 
-celsius_100 = (100-32) * 5/9
+#celsius_100 = (100-32) * 5/9
 
 
 
-print(celsius_100)
-print(type(celsius_100))
-print(int(celsius_100))
+#print(celsius_100)
+#print(type(celsius_100))
+#print(int(celsius_100))
 
 
 
