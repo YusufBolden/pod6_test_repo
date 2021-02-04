10fb4743a6397d6ed1ced7d63d8f756974a118a8
Diana <admin@admins-iMac.lan>
2021-02-04 08:54:23 -0800

    updated temp challenge

---
+++ Diana/nba_challenge2.py
@@ -0,0 +1,42 @@
+jamal_murray_3pts_made = 46 
+
+# Creating variables for number of 3pt shots made by Fred Vanvleet
+fred_vanvleet_3pts_made = 43
+# creating variables for number of 3pt shots made by James Hardeen
+james_hardeen_3pts_made = 37
+
+# print 
+print(fred_vanvleet_3pts_made)
+print(james_hardeen_3pts_made)
+print(jamal_murray_3pts_made)
+
+# Create print statement for Fred Vanvleet
+print(f"In the 2020 NBA playoffs, Fred Vanvleet made {fred_vanvleet_3pts_made} shots.")
+
+# create print statement for Jamal Murray
+print(f"In the 2020 NBA playoffs, Jamal Murray made {jamal_murray_3pts_made} shots.") 
+
+# Create print statement for James Hardeen
+print(f"In the 2020 NBA playoffs, James Hardeen made {james_hardeen_3pts_made} shots.")
+
+# Create variables for attempts made by Fred Vanvleet
+fred_vanvleet_3pt_attempts = 50
+# Create variables for attempts made by Jamal Murray
+jamal_murray_3pt_attempts = 50
+# Create variables for attempts made by James Hardeen
+james_hardeen_3pt_attempts = 50
+
+# print statement for attempts and 3pt shots made
+print(f"In the 2020 NBA playoffs Fred Vanleet made {fred_vanvleet_3pts_made} of {fred_vanvleet_3pt_attempts} attempts.")
+print(f"In the 2020 NBA playoffs Jamal Murray made {jamal_murray_3pts_made} of {jamal_murray_3pt_attempts} attempts.")
+print(f"In the 2020 NBA playoffs James Hardeen mad {james_hardeen_3pts_made} of {james_hardeen_3pt_attempts} attempts.")
+
+# calculate the 3 point percentage of each player
+percentage_fred_vanvleet = 46/50
+print(percentage_fred_vanvleet)
+percentage_jamal_murray = 43/50
+print(percentage_jamal_murray)
+percentage_james_hardeen = 37/50
+print(percentage_james_hardeen)
+
+#print out paragraph with only 1  sentence per line
