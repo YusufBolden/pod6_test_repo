2146e8a1965615cdf6a12391be2f7c4a1166b3e9
YusufBolden <ajb2316@columbia.edu>
2021-02-17 09:56:36 -0500

    Created a for loop progress tracker

---
+++ coin_toss.py
@@ -0,0 +1,12 @@
+from random import randint
+from tqdm.notebook import tdqm, trange
+
+heads = 0
+tails = 0
+
+for flip in tqdm(range(100000)), desc='Coin Flip Progress':
+    toss = randint(0, 1)
+    if toss == 0:
+        heads += 1
+    else:
+        tails += 1
